54d67d40b6a325f3e38d1b3353f515722d660038
AJ Speller <ajspeller@gmail.com>
2018-06-23 21:34:05 -0400

    Create Appbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import NavBar from './components/navbar/NavBar';
+
 import './App.css';
 
 class App extends Component {
   render() {
-    return <div className="App">Hello</div>;
+    return (
+      <MuiThemeProvider>
+        <div>
+          <NavBar />
+        </div>
+      </MuiThemeProvider>
+    );
   }
 }
 
